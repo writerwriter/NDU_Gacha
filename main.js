426d8d9bc0fd0122ea3ebab3634e490020c8bf82
toosyou <toosyou.tw@gmail.com>
2021-04-29 23:55:05 +0800

    clean up

--- main.js
+++ main.js
@@ -148,7 +148,6 @@ app.ticker_gameLoop = function(delta){
     state(delta);
 }
 
-let bolt_open_time = 0;
 let state, gacha_global_time = 0, gacha_result = [0, 1, 2, 2, 1, 2, 0 ,0 ,1 ,2]; // TODO: post gpu4.miplab.org:8999
 let muzzle_flush_open_time = 0;         // TODO: move this in the object
 let gravitational_acceleration = 0.9;   // TODO: turn this into constant
@@ -496,7 +495,7 @@ function onDragMove_chargingHandle(){
 
 function play_single(delta){
     // load bullets
-    if(!game.scenes.gacha_single.bullet.load && hitTestRectangle(game.scenes.gacha_single.bullet, bolt_hitbox)){
+    if(!game.scenes.gacha_single.bullet.load && b.hitTestRectangle(game.scenes.gacha_single.bullet, bolt_hitbox)){
         if(game.scenes.gacha_single.bullet.dragging == false){
             game.scenes.gacha_single.bullet.visible = true;
             game.scenes.gacha_single.bullet.load = true;
@@ -566,17 +565,17 @@ function gacha(delta){
     }
 
     // handle bolt
-    if(bolt_open_time > 2){
+    if(bolt.open_time > 2 && !bolt.force_open){
         bolt.x = 315; // close
     }else{
-        bolt_open_time++;
+        bolt.open_time++;
     }
 
     // handle muzzle_flush
-    if(muzzle_flush_open_time > 2){
+    if(muzzle_flush.open_time > 2){
         muzzle_flush.visible = false;
     }else{
-        muzzle_flush_open_time++;
+        muzzle_flush.open_time++;
     }
 
     // TODO: turn 2434
@@ -590,11 +589,14 @@ function gacha(delta){
 
                 // make bolt open
                 bolt.x = 230;
-                bolt_open_time = 0;
+                bolt.open_time = 0;
 
                 // make muzzle_flush flush
                 muzzle_flush.visible = true;
-                muzzle_flush_open_time = 0;
+                muzzle_flush.open_time = 0;
+
+                if(i == game.shells.length - 1)
+                    bolt.force_open = true;
             }
             
             // update the location of shells
@@ -614,62 +616,11 @@ function gacha(delta){
 
         game.reset();
 
-        bolt_open_time = 0;
         gacha_global_time = 0;
         gacha_result = [0, 1, 2, 2, 1, 2, 0 ,0 ,1 ,2];
-        muzzle_flush_open_time = 0;
+        muzzle_flush.open_time = 0;
         game.loader.load(start);
     }
-}
-
-function hitTestRectangle(r1, r2) {
-  //Define the variables we'll need to calculate
-  let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
-
-  //hit will determine whether there's a collision
-  hit = false;
-
-  //Find the center points of each sprite
-  r1.centerX = r1.x + r1.width / 2 - (r1.anchor ? r1.anchor.x * r1.width : 0);
-  r1.centerY = r1.y + r1.height / 2 - (r1.anchor ? r1.anchor.y * r1.height : 0);
-  r2.centerX = r2.x + r2.width / 2 - (r2.anchor ? r2.anchor.x * r2.width : 0);
-  r2.centerY = r2.y + r2.height / 2 - (r2.anchor ? r2.anchor.y * r2.height : 0);
-
-  //Find the half-widths and half-heights of each sprite
-  r1.halfWidth = r1.width / 2;
-  r1.halfHeight = r1.height / 2;
-  r2.halfWidth = r2.width / 2;
-  r2.halfHeight = r2.height / 2;
-
-  //Calculate the distance vector between the sprites
-  vx = r1.centerX - r2.centerX;
-  vy = r1.centerY - r2.centerY;
-
-  //Figure out the combined half-widths and half-heights
-  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
-  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
-
-  //Check for a collision on the x axis
-  if (Math.abs(vx) < combinedHalfWidths) {
-
-    //A collision might be occuring. Check for a collision on the y axis
-    if (Math.abs(vy) < combinedHalfHeights) {
-
-      //There's definitely a collision happening
-      hit = true;
-    } else {
-
-      //There's no collision on the y axis
-      hit = false;
-    }
-  } else {
-
-    //There's no collision on the x axis
-    hit = false;
-  }
-
-  //`hit` will be either `true` or `false`
-  return hit;
 }
 
 function onTriggerClick(event){
