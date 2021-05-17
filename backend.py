68514d04e6ea800bb494e81072a317401e45594e
toosyou <toosyou.tw@gmail.com>
2021-05-17 13:37:17 +0800

    chance working

--- backend.py
+++ backend.py
@@ -1,5 +1,4 @@
 import numpy as np
-from tinydb import TinyDB
 from flask import Flask, render_template, send_from_directory
 from flask_cors import CORS
 
@@ -8,21 +7,40 @@ app = Flask(__name__,
             static_folder='static')
 CORS(app, support_credentials=True)
 
-db = TinyDB('pool.json')
+pool = {
+    'chance':{
+        'gold': 0.65,
+        'silver': 0.30,
+        'rainbow': 0.05
+    },
+    'remain':{
+        'gold': 999999999,
+        'silver': 190,
+        'rainbow': 30
+    }
+}
 
 def gacha():
-    pass
+    chances = pool['chance']
+    p = [chances['gold'], chances['silver'], chances['rainbow']]
+    result = np.random.choice(['gold', 'silver', 'rainbow'], p=p)
+
+    if pool['remain'][result] == 0: # invalid gacha
+        return gacha()
+    
+    pool['remain'][result] -= 1
+    return ['gold', 'silver', 'rainbow'].index(result)
 
 @app.route("/gacha_single/", methods=['POST'])
 def gacha_single():
     return {
-        'result': np.random.randint(3)
+        'result': gacha()
     }
 
 @app.route("/gacha_ten/", methods=['POST'])
 def gacha_ten():
     return {
-        'result': np.random.randint(3, size=11).tolist()
+        'result': [gacha() for _ in range(11)]
     }
 
 @app.route('/')
